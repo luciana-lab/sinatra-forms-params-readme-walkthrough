460b6aa238fbd76c86e5d1fba171fa8c54853700
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-16 11:06:15 -0400

    commit tree b18b2ff9bbd1a990515046be4c4fa8ba368a37fe

--- app.rb
+++ app.rb
@@ -8,4 +8,10 @@ class App < Sinatra::Base
 
   # Add your post route and action below
 
+  # responds with a 200
+  # displays the params
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
+  end
+
 end
